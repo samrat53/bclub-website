7dd6d426bb21aebee3c0de49c8c3102dd0e7d9fc
samrat53 <samrat05hccs@gmail.com>
2024-06-02 03:53:22 +0530

    footer build

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,24 +1,53 @@
+import { SocialIcons } from "./SocialIcons";
+
 export default function Footer() {
-    return (
-        <div className="footer-container">
-        <div className="footer-content">
-          
-          {/* Add more footer content as needed */}
+  return (
+    <div className="mt-[50rem]">
+      <footer className=" rounded-lg shadow m-4 relative z-50">
+        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <a
+              href="/"
+              className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+            >
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              © Business Club NIT Agartala
+              </span>
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+              <li>
+                <a href="#" className="hover:underline me-4 md:me-6">
+                  LinkedIn
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline me-4 md:me-6">
+                  Twitter
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline me-4 md:me-6">
+                  Instagram
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="custom-shape-divider-bottom-1716753471">
-          <svg
-            data-name="Layer 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1200 120"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M741,116.23C291,117.43,0,27.57,0,6V120H1200V6C1200,27.93,1186.4,119.83,741,116.23Z"
-              className="shape-fill"
-            ></path>
-          </svg>
-        </div>
+      </footer>
+      <div className="custom-shape-divider-bottom-1717277712">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M741,116.23C291,117.43,0,27.57,0,6V120H1200V6C1200,27.93,1186.4,119.83,741,116.23Z"
+            className="shape-fill"
+          ></path>
+        </svg>
+        <h1></h1>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
